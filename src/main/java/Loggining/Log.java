894a3fa2677a6138c1b8d3f8ae60aeac548ef731
ViktorV <verkhorubovvv@mail.ru>
2024-04-01 00:11:14 +0800

    1213445

---
+++ src/main/java/Loggining/Log.java
@@ -0,0 +1,22 @@
+package Loggining;
+
+
+public interface Log {
+
+    /**
+     * Запись выражения в файл
+     * @param expression - математическое выражение в виде отформатированной строки
+     * @param nameFile - путь к файлу
+     */
+    public void writeFile(String expression, String nameFile);
+
+    /**
+     * Метод получения математического выражения в виде отформатированной строки
+     * @param x - первое число
+     * @param y - второе число
+     * @param result - результат
+     * @param operation - математическое действие
+     * @return - отформатированная строка
+     */
+    public String getExpression(Double x, Double y, Double result, Integer operation);
+}
---
+++ src/main/java/Operations/Subtract.java
@@ -0,0 +1,19 @@
+package Operations;
+
+public class Subtract extends Operations.Model {
+
+    @Override
+    public Double result() {
+        return x - y;
+    }
+
+    @Override
+    public void setX(Double value) {
+        super.x = value;
+    }
+
+    @Override
+    public void setY(Double value) {
+        super.y = value;
+    }
+}
